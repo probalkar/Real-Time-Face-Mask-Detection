64cec41d16e4669c7adbf30cb3440da4dfaf8de5
Probal Kar <74421343+probalkar3123@users.noreply.github.com>
2023-11-07 19:34:54 +0530

    Added files of the project

---
+++ app.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from deepface import DeepFace
+
+# Load the trained mask detection model
+model = load_model("mask_detection_model.h5")
+
+# Load the pre-trained face detection model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Create a function for real-time mask detection
+def detect_mask(frame):
+    # Preprocess the frame
+    resized_frame = cv2.resize(frame, (128, 128))
+    resized_frame = img_to_array(resized_frame)
+    resized_frame = preprocess_input(resized_frame)
+    resized_frame = np.expand_dims(resized_frame, axis=0)
+
+    # Perform prediction
+    predictions = model.predict(resized_frame)
+    return predictions
+
+# Streamlit web app
+st.title("Real-time Mask Detection")
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    st.error("Error: Could not open webcam.")
+
+else:
+    stframe = st.empty()
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            st.error("Error: Unable to capture frame.")
+            break
+        
+        # Perform mask detection
+        predictions = detect_mask(frame)
+        label = "Mask" if np.argmax(predictions) == 1 else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        
+        # Display the frame with the label
+        for (x, y, w, h) in faces:
+            face = frame[y:y + h, x:x + w]
+
+            # Perform mask detection
+            # ... Your mask detection code here ...
+            predictions = detect_mask(frame)
+            label = "Mask" if np.argmax(predictions) == 1 else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+            # If no mask is detected, estimate age and gender
+            if label == "No Mask":
+                results = DeepFace.analyze(frame[y:y+h, x:x+w], actions=['age', 'gender'], enforce_detection=False)
+                results = results[0]
+                age = results['age']
+                gender = results['dominant_gender']
+                # print(results)
+                # print(type(results[0]))
+
+                # Display age and gender estimation
+                cv2.putText(frame, f'Age: {age:.1f} years', (x, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+                cv2.putText(frame, f'Gender: {gender}', (x, y - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+            # Display mask detection result
+            # ... Your mask detection display code here ...
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, label, (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+        
+        # Convert the frame to RGB for Streamlit
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Display the frame using Streamlit
+        stframe.image(frame_rgb, channels="RGB", use_column_width=True)
+        
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ face_mask_detection.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+# Initialize the base MobileNetV2 model
+base_model = MobileNetV2(weights="imagenet", include_top=False, input_shape=(128, 128, 3))
+
+# Construct the head of the model that will be placed on top of the base model
+head_model = base_model.output
+head_model = AveragePooling2D(pool_size=(4, 4))(head_model)
+head_model = Flatten(name="flatten")(head_model)
+head_model = Dense(128, activation="relu")(head_model)
+head_model = Dropout(0.5)(head_model)
+head_model = Dense(2, activation="softmax")(head_model)
+
+# Combine the base model and the head model
+model = Model(inputs=base_model.input, outputs=head_model)
+
+# Freeze the layers in the base model
+for layer in base_model.layers:
+    layer.trainable = False
+
+# Compile the model
+opt = Adam(lr=1e-4)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+# Data augmentation and preprocessing
+train_datagen = ImageDataGenerator(
+    rescale=1.0/255.0,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode="nearest"
+)
+
+# Define the path to the data directory
+data_directory = "data"
+
+# Generate data from the directory structure
+train_generator = train_datagen.flow_from_directory(
+    data_directory,
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode="categorical",
+    classes=["without_mask", "with_mask"]  # Order of classes matters
+)
+
+# Training the model
+model.fit(
+    train_generator,
+    steps_per_epoch=len(train_generator),
+    epochs=20
+)
+
+# Save the model
+model.save("mask_detection_model.h5")
