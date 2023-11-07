3e4a50343b2ad7a4bc2ff6a98be6f9d46cefd982
Probal Kar <74421343+probalkar3123@users.noreply.github.com>
2023-11-07 20:57:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,15 @@ st.title("Real-time Mask Detection")
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
 
-    flipped = img[::-1,:,:]
+    # flipped = img[::-1,:,:]
 
     return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
 
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+webrtc_streamer(key="example",
+        video_frame_callback=video_frame_callback,
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        media_stream_constraints={"video": True, "audio": False})
 
 # Open the webcam
 cap = cv2.VideoCapture(0)
